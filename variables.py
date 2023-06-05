f9a1c77e510030be145c67d0b52b2d4aac60f7ca
Nantheesan <135431337+Nantheesan@users.noreply.github.com>
2023-06-05 09:23:44 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Yuvarasa\AppData\Local\Programs\Python\Python311\caterpillar game.py
+
+================================ RESTART: Shell ================================
+>>> age=12
+>>> print(age)
+12
+>>> weight=38
+>>> print(weight)
+38
+>>> height=128
+>>> print(height)
+128
