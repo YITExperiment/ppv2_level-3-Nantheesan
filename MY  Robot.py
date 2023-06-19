4b51e9c4606b98726320087597469c28dcbaa12c
Nantheesan <135431337+Nantheesan@users.noreply.github.com>
2023-06-19 14:59:45 +0530

    Add files via upload

--- MY  Robot.py
+++ MY  Robot.py
@@ -54,16 +54,14 @@ rectangle(80,50,'lightblue')
 #eyes
 t.goto(-60, 160)
 rectangle(30,10,'white')
-t.goto(-60, 160)
+t.goto(-55,155)
 rectangle(5,5,'black')
-t.goto(-45,155)
+t.goto(-40,155)
 rectangle(5,5,'black')
 
 #mouth()
 t.goto(-65,135)
-t.right(5)
 rectangle(40,5,'yellow')
-
 t.hideturtle()
        
     
